99216789a090b4448b545ed4eebc7d96bac4a93a
Sarzhin Ivan <4228815@mail.ru>
2022-01-29 09:56:58 +0300

    add Person.java

---
+++ lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String sex = "male";
+    String name = "Bobby";
+    int height = 185;
+    int weight = 85;
+    int age = 85;
+
+    public void run() {
+        System.out.println(name + " is run");
+    }
+    
+    public void sit() {
+        System.out.println(name + " is sit");
+    }
+    
+    public void go() {
+        System.out.println(name + " is go");
+    }
+    
+    public void speak() {
+        System.out.println(name + " is speak");
+    }
+    
+    public void learnJava() {
+        System.out.println(name + " is learn Java");
+    }
+}
