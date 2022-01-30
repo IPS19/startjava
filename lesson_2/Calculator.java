350395eb6fa59423d8d63cc7d92de6668be25789
Sarzhin Ivan <4228815@mail.ru>
2022-01-30 18:55:48 +0300

    temporary fix CalculatorTest

--- lesson_2/Calculator.java
+++ lesson_2/Calculator.java
@@ -4,38 +4,30 @@ public class Calculator {
     public static void calc() {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Введите первое число");
-        int firstNumber = scan.nextInt();
-
-        System.out.println("введите действие");
-        char action = scan.next().charAt(0);
-
-        System.out.println("Введите второе число");
-        int secondNumber = scan.nextInt();
         int result = 0;
-        switch (action) {
+        switch (CalculatorTest.action) {
             case '*':
-                result = firstNumber * secondNumber;
+                result = CalculatorTest.firstNumber * CalculatorTest.secondNumber;
                 break;
             case '/':
-                result = firstNumber / secondNumber;
+               result = CalculatorTest.firstNumber / CalculatorTest.secondNumber;
                 break;
             case '+':
-                result = firstNumber + secondNumber;
+                result = CalculatorTest.firstNumber + CalculatorTest.secondNumber;
                 break;
             case '-':
-                result = firstNumber - secondNumber;
+                result = CalculatorTest.firstNumber - CalculatorTest.secondNumber;
                 break;
             case '%':
-                result = firstNumber % secondNumber;
+                result = CalculatorTest.firstNumber % CalculatorTest.secondNumber;
                 break;
             case '^':
-                result = firstNumber;
-                while (secondNumber > 1) {
-                    result *= firstNumber;
-                    secondNumber--;
+                result = CalculatorTest.firstNumber;
+                while (CalculatorTest.secondNumber > 1) {
+                    result *= CalculatorTest.firstNumber;
+                    CalculatorTest.secondNumber--;
                 }
         }
-        System.out.println("результат: " + (double)result);
+        System.out.println("результат: " + result);
     }
 }
--- lesson_2/CalculatorTest.java
+++ lesson_2/CalculatorTest.java
@@ -1,21 +1,38 @@
 import java.util.Scanner;
 
 public class CalculatorTest {
+    static int firstNumber;
+    static int secondNumber;
+    static char action;
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String userChoice;
         while (true) {
+            System.out.println("Введите первое число");
+            firstNumber = scanner.nextInt();
+
+            System.out.println("введите действие");
+            action = scanner.next().charAt(0);
+
+            System.out.println("Введите второе число");
+            secondNumber = scanner.nextInt();
+
             Calculator.calc();
+
             System.out.println("Хотите продолжить вычисления? [yes/no]:");
-            Scanner scanner = new Scanner(System.in);
-            String userChoice = scanner.nextLine();
+
+            userChoice = scanner.nextLine();
 
             if (userChoice.equals("yes")) {
                 continue;
             } else if (userChoice.equals("no")) {
                 break;
-            } else do {
+            } else
+                do {
                 System.out.println("неверный ввод, повторите [yes/no]");
-                userChoice = scanner.nextLine();
-                if(userChoice.equals("yes") || userChoice.equals("no"))
+                    userChoice = scanner.nextLine();
+                if (userChoice.equals("yes") || userChoice.equals("no"))
                     break;
             } while (true);
             if(userChoice.equals("no"))
