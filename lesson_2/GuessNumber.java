05e5397b5503bd1f6254c9ff44a4607ec0c380ac
Sarzhin Ivan <4228815@mail.ru>
2022-02-18 17:58:30 +0300

    fix GuessNumber, GuessNumberTest 18/02

--- lesson_2/GuessNumber.java
+++ lesson_2/GuessNumber.java
@@ -2,21 +2,18 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class GuessNumber {
-    Player player1;
-    Player player2;
+    private Player player1;
+    private Player player2;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
         this.player2 = player2;
     }
 
-    public void startGame() {
-        Random random = new Random();
+    public void start() {
         Scanner scanner = new Scanner(System.in);
-        String userChoice = "";
-
-        int generatedNumber;
-        generatedNumber = random.nextInt(101) + 1;
+        Random random = new Random();
+        int generatedNumber = random.nextInt(100) + 1;
 
         while (true) {
             System.out.println(player1.getName() + ", введи число");
@@ -26,22 +23,10 @@ public class GuessNumber {
             } else if (player1.getNumber() > generatedNumber) {
                 System.out.println("Данное число больше того, что загадал компьютер");
             } else if (player1.getNumber() == generatedNumber) {
-                System.out.println(player1.getName() + " угадал!" + "\n" + "Хотите продолжить игру? [yes/no]:");
-                userChoice = scanner.next();
-                if (userChoice.equals("yes")) {
-                    generatedNumber = random.nextInt(101) + 1;
-                    continue;
-                } else {
-                    while (!userChoice.equals("no")) {
-                        System.out.println("неверный ввод, повторите [yes/no]:");
-                        userChoice = scanner.next();
-                        if (userChoice.equals("yes"))
-                            break;
-                    }
-                    break;
-                }
+                System.out.println(player1.getName() + " угадал!");
+                generatedNumber = random.nextInt(100) + 1;
+                break;
             }
-
             System.out.println(player2.getName() + ", введи число");
             player2.setNumber(scanner.nextInt());
             if (player2.getNumber() < generatedNumber) {
@@ -49,20 +34,9 @@ public class GuessNumber {
             } else if (player2.getNumber() > generatedNumber) {
                 System.out.println("Данное число больше того, что загадал компьютер");
             } else if (player2.getNumber() == generatedNumber) {
-                System.out.println(player2.getName() + " угадал!" + "\n"+ "Хотите продолжить игру? [yes/no]:");
-                userChoice = scanner.next();
-                if (userChoice.equals("yes")) {
-                    generatedNumber = random.nextInt(101) + 1;
-                    continue;
-                } else {
-                    while (!userChoice.equals("no")) {
-                        System.out.println("неверный ввод, повторите [yes/no]:");
-                        userChoice = scanner.next();
-                        if (userChoice.equals("yes"))
-                            break;
-                    }
-                    break;
-                }
+                System.out.println(player2.getName() + " угадал!");
+                generatedNumber = random.nextInt(100) + 1;
+                break;
             }
         }
     }
--- lesson_2/GuessNumberTest.java
+++ lesson_2/GuessNumberTest.java
@@ -1,17 +1,34 @@
 import java.util.Scanner;
 
 public class GuessNumberTest {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String userChoice = "";
 
-        System.out.println("Угадай число" + "\n" + "Введите имя первого игрока");
+        System.out.println("Угадай число");
+        System.out.println("Введите имя первого игрока");
         Player player1 = new Player(scanner.next());
 
         System.out.println("Введите имя второго игрока");
         Player player2 = new Player(scanner.next());
 
-        GuessNumber guessNumber = new GuessNumber(player1, player2);
-        guessNumber.startGame();
+        while (true) {
+            GuessNumber game = new GuessNumber(player1, player2);
+            game.start();
+            System.out.println("Хотите продолжить игру? [yes/no]:");
+            userChoice = scanner.next();
+            if (userChoice.equals("yes")) {
+                continue;
+            } else {
+                while (!userChoice.equals("no")) {
+                    System.out.println("неверный ввод, повторите [yes/no]:");
+                    userChoice = scanner.next();
+                    if (userChoice.equals("yes"))
+                        break;
+                }
+                if (userChoice.equals("no"))
+                    break;
+            }
+        }
     }
 }
