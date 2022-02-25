3d73e1ed841d482eaa3f92a582b69f8b0f0e356f
IPS19 <i89214228815@gmail.com>
2022-02-25 15:28:49 +0300

    Calculator fix, lesson_4

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,31 +1,29 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 public class Calculator {
-    public static String calc(String expression) {
-        int firstNumber;
-        int secondNumber;
-        char action;
+    public static int calc(String expression) {
+
         String[] expressionArray = expression.split(" ");
-        firstNumber = Integer.parseInt(expressionArray[0]);
-        secondNumber = Integer.parseInt(expressionArray[2]);
-        action = expressionArray[1].charAt(0);
+        int firstNumber = Integer.parseInt(expressionArray[0]);
+        int secondNumber = Integer.parseInt(expressionArray[2]);
+        char action = expressionArray[1].charAt(0);
 
         switch (action) {
             case '*':
-                return Integer.toString(Math.multiplyExact(firstNumber, secondNumber));
+                return Math.multiplyExact(firstNumber, secondNumber);
             case '/':
-                return Integer.toString(firstNumber / secondNumber);
+                return firstNumber / secondNumber;
             case '+':
-                return Integer.toString((Math.addExact(firstNumber, secondNumber)));
+                return Math.addExact(firstNumber, secondNumber);
             case '-':
-                return Integer.toString((Math.subtractExact(firstNumber, secondNumber)));
+                return Math.subtractExact(firstNumber, secondNumber);
             case '%':
-                return Integer.toString(firstNumber % secondNumber);
+                return firstNumber % secondNumber;
             case '^':
-                return Integer.toString((int)Math.pow(firstNumber, secondNumber));
+                return (int) Math.pow(firstNumber, secondNumber);
             default:
                 System.out.println("неверно введено действие");
-                return "0";
+                return 0;
         }
     }
 }
